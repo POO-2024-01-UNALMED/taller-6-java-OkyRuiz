9d6cb6f68fa7fed9ad5f363b45896139c7c1b95c
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-04-01 12:26:21 -0500

    Clase vehiculo

---
+++ src/main/java/vehiculos/Vehiculo.java
@@ -0,0 +1,13 @@
+public class Vehiculo {
+
+    protected String placa;
+    protected int puertas;
+    protected double VelocidadMaxima;
+    protected String nombre;
+    protected double precio;
+    protected double peso;
+    protected String traccion;
+    protected Fabricante fabricante;
+
+    
+}
