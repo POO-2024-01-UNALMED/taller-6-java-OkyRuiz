d65aaf770a22f79b5f9f1f3784aa281576654fdd
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-04-01 14:28:54 -0500

    Clase vehiculo

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -1,5 +1,27 @@
 package vehiculos;
 
 public class Vehiculo {
+    private static int cantidadVehiculos=0;
+	private String placa;
+	private int puertas;
+	private int velocidadMaxima;
+	private String nombre;
+	private int precio;
+	private int peso;
+	private String traccion;
+	private Fabricante fabricante;
+	
+	
+	public Vehiculo(String placa, int puertas, int velocidad, String nombre, int precio, int peso, String traccion, Fabricante fabri) {
+		
+		this.placa=placa;
+		this.puertas= puertas;
+		this.velocidadMaxima= velocidad;
+		this.nombre= nombre;
+		this.precio= precio;
+		this.peso= peso;
+		this.traccion= traccion;
+		this.fabricante= fabri;
+	}
 
 }
